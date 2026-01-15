f0ec402b39e4d90d4cbc3839eaf07e563514926e
Jorge <sonic.medinaperez@gmail.com>
2026-01-15 16:38:29 -0400

    agrega el primer modulo

---
+++ addons/Inventory/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Inventory",
+    "version": "1.0",
+    "category": "General",
+    "summary": "Inventory",
+    "description": "Inventory",
+    "author": "Jorge",
+    "depends": [],
+    "data": [
+    
+    ],
+    "installable": True,
+    "application": True,
+}
